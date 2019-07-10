d67d7eea8adc7831f69b91f0ec084894eaec2f41
ajisetyawan <ajisetyawan58@gmail.com>
2019-07-10 16:03:07 +0700

    TA_MobileProgramming

---
+++ app/src/main/java/com/ajisetyawan/ta_mobile_programming/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ajisetyawan.ta_mobile_programming
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
